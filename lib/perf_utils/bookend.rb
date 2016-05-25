0937ef2ed5c2ffdb7049927f7363c631a78d25ba
Keenan Brock <keenan@thebrocks.net>
2016-05-24 22:21:14 -0400

    bookend instance, uses new perf util classes

--- lib/perf_utils/bookend.rb
+++ lib/perf_utils/bookend.rb
@@ -3,6 +3,9 @@ require 'objspace'
 require 'singleton'
 require 'logger'
 require 'json'
+require 'perf_utils/rack_storage'
+require 'perf_utils/printer'
+
 
 class Bookend
   class SimpleFormatter < Logger::Formatter
@@ -39,6 +42,40 @@ class Bookend
   def self.track(name, &block)
     instance.track(name, &block)
   end
+    base_url = options[:base_url] || "http://localhost:3000"
+    # base_file = options[:base_file] || defined?(Rails) ? Rails.root.join("public") : "."
+    env = {'RACK_MINI_PROFILER_ORIGINAL_SCRIPT_NAME' => base_url}
+
+    page_struct = storage.current(env).page_struct
+    start = Time.now
+    #page_struct[:user] = config.user_provider.call(env) # needed?
+    page_struct[:name] = name
+    yield
+
+    page_struct[:root].record_time((Time.now - start) * 1000)
+#    storage.set_unviewed(page_struct[:user], page_struct[:id])
+    storage.save(page_struct)
+    page_struct[:id]
+  ensure
+    ::Rack::MiniProfiler.current = nil
+  end
+
+  def print(pages)
+    pages = pages.map { |id| storage.load(id) }
+    page_groups = pages.group_by { |page| page[:name] =~ /^(.*[^0-9])[0-9]+$/ ; $1 }.values
+    printer.print_group(page_groups)
+  end
+
+  def printer
+    @printer ||= ::PerfUtils::Printer.new.tap do |printer|
+      printer.display_children = true
+      printer.display_sql = true
+    end
+  end
+
+  def storage
+    @storage ||= ::PerfUtils::RackStorage.new
+  end
 end
 
 def thrice(name, count = 1, &block)
@@ -51,15 +88,20 @@ def thrice(name, count = 1, &block)
   end
 end
 
-def twosands(name = "no name", count = 2, open_all = nil, &block)
+def bookend(name = "no name", count = 1, open_all = nil, &block)
   gen = false
   if open_all.to_s =~ /gen/
     open_all = false
     gen = true
   end
-  x = count.times.collect { |i| sand2("#{name}-#{i+1}", :normal, open_all.nil? ? i > 0 : open_all, &block) }.inspect
+  gp = Bookend.instance
+  x = count.times.collect do |i|
+    #sand2("#{name}-#{i+1}", :normal, open_all.nil? ? i > 0 : open_all, &block)
+    gp.capture("#{name}-#{i+1}", &block)
+  end
   puts "beer gen_perf #{x}"
-  puts `gen_perf #{x}` if gen
+  gp.print(x) if gen
+#  puts `gen_perf #{x}` if gen
   x
 end
 
@@ -89,9 +131,6 @@ end
 if false
 
 Metric::Capture::perf_capture_timer
-
-Bookend.track("perf_capture_health_check") { Metric::Capture.perf_capture_health_check }
-QueryCounter2.track { Metric::Capture.perf_capture_health_check }
 bookend("perf_capture_health_check") do
   Metric::Capture.perf_capture_health_check
 end
